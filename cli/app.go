2c08c0433d638c4b5e14ca29b88b68421d9dc4ef
Kevin Webster <kwebster@renewfinancial.com>
2016-09-03 14:13:22 -0700

    Setting source directory, changed old source to get

--- cli/app.go
+++ cli/app.go
@@ -2,14 +2,23 @@ package cli
 
 import (
 	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/controlroom/lincoln/metadata"
 	"github.com/spf13/cobra"
 )
 
+var appGetAll bool
+
 func init() {
+	appGetCmd.Flags().BoolVar(&appGetAll, "all", false, "Clone all apps")
+
 	appCmd.AddCommand(appStatusCmd)
-	appCmd.AddCommand(appSourceCmd)
+	appCmd.AddCommand(appGetCmd)
 	appCmd.AddCommand(appListCmd)
+	appCmd.AddCommand(appSourceCmd)
 	RootCmd.AddCommand(appCmd)
 }
 
@@ -43,10 +52,48 @@ func appList(c *cobra.Command, args []string) {
 }
 
 // ===  Source  =================================================================
-//
 var appSourceCmd = &cobra.Command{
-	Use:   "source [name] [destination]",
-	Short: "Pull git source for application",
+	Use:   "source [path]",
+	Short: "Get/set project source directory",
+	Run:   appSource,
+}
+
+func exists(path string) error {
+	_, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func appSource(c *cobra.Command, args []string) {
+	if len(args) == 0 {
+		source := metadata.GetMeta("app:currentSource")
+		if source == "" {
+			fmt.Println("Source not set, please pass source path")
+		} else {
+			fmt.Printf("Source: %v", source)
+		}
+	} else {
+		source, err := filepath.Abs(args[0])
+		if err != nil {
+			panic(err)
+		}
+
+		if err = exists(source); err == nil {
+			metadata.PutMeta("app:currentSource", source)
+			fmt.Printf("Source set: %v", source)
+		} else {
+			fmt.Printf("%v is not a directory", source)
+		}
+	}
+}
+
+// ===  Get  =================================================================
+//
+var appGetCmd = &cobra.Command{
+	Use:   "get [apps]",
+	Short: "Git clone application into projects folder",
 	Long: `
 Pull project source code into local destination. The path you pass into
 destination should be the root folder for all your applications. Lincoln will
@@ -54,15 +101,23 @@ create a project folder and clone the project for you. Lincoln will also
 remember where you cloned the project, so you can swap in a development version
 to a stack with ease.
 	`,
-	RunE: appSource,
+	RunE: appGet,
 }
 
-func appSource(c *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		return errors.New("Missing app name & destination")
-	} else if len(args) == 1 {
-		return errors.New("Missing destination")
+func appGet(c *cobra.Command, args []string) error {
+	var apps []string
+
+	if appGetAll == false && len(args) == 0 {
+		return errors.New("Missing app name")
 	}
+
+	if appGetAll {
+		apps = []string{"__all"}
+	} else {
+		apps = args
+	}
+
+	fmt.Println(apps)
 
 	return nil
 }
